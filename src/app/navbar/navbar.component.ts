0beb7c9de958fbf9aa26ab9d56c9a43b47aa2dfc
Sagar NH <sagarnh27@gmail.com>
2023-02-21 22:28:34 +0530

    Changes Related UI

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
-
+export class NavbarComponent implements OnInit {
+showIcon: boolean = true; 
+ngOnInit(){
+ if(window.innerWidth > 992) {
+  this.showIcon =  false;
+ } else {
+  this.showIcon =  true;
+ }
+}
+@HostListener('window:resize', ['$event']) 
+onWindowResize() {
+  if(window.innerWidth > 992) {
+    this.showIcon =  false;
+   } else {
+    this.showIcon =  true;
+   }
+}
+enableMenu () {
+  if(!(window.innerWidth > 992)) {
+    this.showIcon = ! this.showIcon;
+  }
+}
 }
